6402e488fcf1459ebdfe51957d888430d46159f8
PedroLeGarcia <bstoolk@gmail.com>
2022-03-22 00:23:00 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,10 @@
+// This is Kane West... Edu tem medo e treme
+window.onload = function (){
+    document.querySelector("#alerta1").addEventListener("click", function(){
+    function retorno(){
+
+    }
+    navigator.notification.alert("Tem medo e treme?", retorno, "MISTER M", "sim");
+    });
+}
+
